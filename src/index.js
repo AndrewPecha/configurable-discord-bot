50a10b4dbd12148d62c1661c20e8773b3c8a7a13
Andrew Pecha <andrew.pecha@gmail.com>
2022-07-23 21:53:33 -0500

    extract some functions and add comments

--- src/index.js
+++ src/index.js
@@ -6,24 +6,40 @@ const fs = require('fs');
 let bot = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
 
-bot.on("ready", function () {
-    console.log("Connected");
-    console.log("Logged in as: ");
-    console.log(bot.user.username + " - (" + bot.user.id + ")");
-    fs.stat('bad-words.json', function(err, stat) {
+function CreateBadWordsFileIfNotExists() {
+    let configFileName = 'bad-words.json';
+
+    fs.stat(configFileName, function (err, stat) {
         if (err == null) {
             console.log('bad word file exists');
         } else if (err.code === 'ENOENT') {
             // file does not exist
-            fs.writeFile('bad-words.json', "{}", function() {});
+            fs.writeFile(configFileName, "{}", function () {
+            });
         } else {
             console.log('Some other error: ', err.code);
         }
     });
+}
+
+bot.on("ready", function () {
+    console.log("Connected");
+    console.log("Logged in as: ");
+    console.log(bot.user.username + " - (" + bot.user.id + ")");
+    CreateBadWordsFileIfNotExists();
+    console.log("I'm ready!")
 });
 
 bot.on("messageCreate", function (message) {
     console.log(message.content);
+    if(message.content.charAt(0) === '!'){
+        //this if needs to check the first 3 words after exclamation are "add forbidden word"
+        if(message.content.slice(1).toLowerCase() === "add forbidden word") {
+            console.log("adding bad word")
+            //get bad words from config file based on server id
+            //add to server bad words if not exists already
+        }
+    }
 });
 
 bot
