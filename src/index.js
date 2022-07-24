c01c5ababc220267f1855e0dc44e239107985a8e
Andrew Pecha <andrew.pecha@gmail.com>
2022-07-23 21:39:15 -0500

    initial bot setup is complete

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 let auth = require("./auth.json");
-const { Intents, Client } = require("discord.js");
+const {Client, GatewayIntentBits} = require("discord.js");
+const fs = require('fs');
 
-let bot = new Client({
-    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
-});
+
+let bot = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+
 
 bot.on("ready", function () {
     console.log("Connected");
     console.log("Logged in as: ");
     console.log(bot.user.username + " - (" + bot.user.id + ")");
+    fs.stat('bad-words.json', function(err, stat) {
+        if (err == null) {
+            console.log('bad word file exists');
+        } else if (err.code === 'ENOENT') {
+            // file does not exist
+            fs.writeFile('bad-words.json', "{}", function() {});
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+    });
 });
 
 bot.on("messageCreate", function (message) {
-    console.log(message);
+    console.log(message.content);
 });
 
 bot
